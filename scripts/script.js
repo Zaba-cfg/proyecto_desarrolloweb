852414e7e485910e7d73dc6087828308d21f943a
Agustin <agu-zaballa@hotmail.com>
2022-11-15 09:53:18 -0300

    Retoques de estilos

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/*let num1 = prompt("Ingrese un número")
+let num2 = prompt("Ingrese un número")
+
+if (num1 == num2) {
+    console.log("¡Los números son iguales!")
+    alert("LOS NUMEROS SON IGUALES")
+} else {
+    console.log("¡Los números son distintos!")
+    alert("LOS NUMEROS SON DISTINTOS")
+}*/
+
+/*let ingresarNumero = prompt("Ingrese el número a multiplicar para conseguir su tabla")
+for (let i = 0; i <= 10; i++) {
+    let resultado = ingresarNumero * i
+    alert(ingresarNumero + " x " + i + " = " + resultado)
+}*/
